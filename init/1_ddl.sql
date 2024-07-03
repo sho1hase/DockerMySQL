99426405bc35417e332d09a011233aae3355e4c5
hasegawa shoichi <pg2012x@gmail.com>
2024-07-03 23:02:06 +0900

    ddl commit

--- init/1_ddl.sql
+++ init/1_ddl.sql
@@ -3,5 +3,13 @@ CREATE DATABASE IF NOT EXISTS mine;
 CREATE TABLE IF NOT EXISTS mine.idol(
   `id` int(11) NOT NULL  AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
+  `mail` varchar(50) ,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS mine.test(
+  `id` int(11) NOT NULL  AUTO_INCREMENT,
+  `name` varchar(50) ,
+  `mail` varchar(50) ,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
